9318ea56015be63587a761e43e6da2f60ffdcd31
chris <chris@DESKTOP-NTTCC6T>
2025-02-03 01:46:07 -0700

    final

--- BetterCalculator/betterCalculator.vb
+++ BetterCalculator/betterCalculator.vb
@@ -2,45 +2,88 @@
 'Spring 2025
 'RCET2265
 'Operators Example
-'https://github.com/Christopher-isu/SayMyName.git
+'https://github.com/Christopher-isu/BetterCalculator.git
 
 Option Explicit On 'forces all variables to be declared
-Module betterCalculator
+
+Module BetterCalculator
+
+    Function enterNumber() As Integer
+        Dim number As Integer
+        Dim input As String
+        Do
+            Console.WriteLine("Choose a Number:")
+            input = Console.ReadLine()
+
+            If input.ToUpper() = "Q" Then
+                Console.WriteLine("You entered " & number)
+                Console.WriteLine("Have a nice day")
+                Environment.Exit(0)
+            End If
+
+            Try
+                number = CInt(input)
+            Catch ex As Exception
+                Console.WriteLine($"You entered ""{input}"", please enter a whole number.")
+            End Try
+        Loop Until IsNumeric(input)
+
+        Console.WriteLine("You entered " & number)
+        Return number
+    End Function
+
+    Function selectOperation() As Integer
+        Dim operation As Integer
+        Dim input As String
+        Do
+            Console.WriteLine("Choose one of the following options:")
+            Console.WriteLine("1. Add")
+            Console.WriteLine("2. Subtract")
+            Console.WriteLine("3. Multiply")
+            Console.WriteLine("4. Divide")
+            input = Console.ReadLine()
+
+            If input.ToUpper() = "Q" Then
+                Console.WriteLine("You entered " & input)
+                Console.WriteLine("Have a nice day")
+                Environment.Exit(0)
+            End If
+
+            Try
+                operation = CInt(input)
+            Catch ex As Exception
+                Console.WriteLine($"You entered ""{input}"", please enter a whole number.")
+            End Try
+        Loop Until operation >= 1 And operation <= 4
+
+        Return operation
+    End Function
+
     Sub Main()
-        ' Prompt user for the first number
-        Console.WriteLine("This is a simple calculator example that can perform simple operations on two integers.")
-        Console.WriteLine("Please enter the first number:")
+        Do
+            Console.WriteLine("Please enter two numbers. Enter 'Q' at any time to quit")
 
-        ' Save the frist number in a variable
-        Dim firstNumber As Integer = Console.ReadLine()
+            Dim result As Double
+            Dim num1 As Integer = enterNumber()
+            Dim operation As Integer = selectOperation()
+            Dim num2 As Integer = enterNumber()
 
-        ' Prompt user for the first number
-        Console.WriteLine("Enter the second number:")
+            Select Case operation
+                Case 1
+                    result = (num1) + (num2)
+                    Console.WriteLine($"{num1} + {num2} = {result}")
+                Case 2
+                    result = (num1) - (num2)
+                    Console.WriteLine($"{num1} - {num2} = {result}")
+                Case 3
+                    result = (num1) * (num2)
+                    Console.WriteLine($"{num1} * {num2} = {result}")
+                Case 4
+                    result = (num1) / (num2)
+                    Console.WriteLine($"{num1} / {num2} = {result}")
+            End Select
 
-        ' Save the second number in a variable
-        Dim secondNumber As Integer = Console.ReadLine()
-
-        ' Prompt user for the first number
-        Console.WriteLine("Choose operation:")
-        Console.WriteLine("1. Sum")
-        Console.WriteLine("2. Product")
-
-        ' Save the operation in a variable
-        Dim operation As Integer = Console.ReadLine()
-
-        Select Case operation
-            Case 1
-                Console.WriteLine("The sum is: " & (firstNumber + secondNumber))
-            Case 2
-                Console.WriteLine("The product is: " & (firstNumber * secondNumber))
-            Case Else
-                Console.WriteLine("Invalid operation.")
-        End Select
-
-        ' Keep console open untill button pressed
-        Console.WriteLine("Press any key to exit.")
-        Console.ReadKey()
-
+        Loop Until False
     End Sub
 
 End Module
